11b0a8322a01af3cb7f9c04f71a6d223891b46d1
곽채연 <cheyeon001214@naver.com>
2024-01-09 19:22:29 +0900

    [BOJ] AxB

---
+++ codus/week_02/AxB_10998.java
@@ -0,0 +1,18 @@
+package codus.week_02;
+
+import java.util.Scanner;
+
+public class AxB_10998  {
+    public static void main(String[] args) {
+        int a,b;
+        int c;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = a*b;
+        //2scan.close();
+        System.out.println(c);
+    }
+
+    
+}
