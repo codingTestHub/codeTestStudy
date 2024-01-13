8c0d7ee3721ba3939ee29f0a138539453bd2da2d
inthyes <hyesoo0806@gmail.com>
2024-01-13 17:05:31 +0900

    [BOJ] 2417/정수제곱근/실버4

---
+++ inthyes/week_04/2417_정수 제곱근.py
@@ -0,0 +1,12 @@
+# 이분탐색
+n = int(input())
+s = 0 # 시작점 0으로 지정
+e = n # 끝점 n으로 지정
+
+while s <= e: # 시작점이 끝점보다 커지면 끝나는 반복문
+    mid = (s + e) // 2
+    if mid ** 2 < n:
+        s = mid + 1
+    else:ㅎ
+        e = mid - 1
+print(s)
