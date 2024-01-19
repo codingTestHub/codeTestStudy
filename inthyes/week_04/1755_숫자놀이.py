b2bf73436e5276d2616ce60c54ada3cc9c38ea2e
inthyes <hyesoo0806@gmail.com>
2024-01-19 16:41:46 +0900

    [BOJ] 1755/숫자놀이/실버4

---
+++ inthyes/week_04/1755_숫자놀이.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+dic = {'1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
+li = []
+for i in range(n, m+1):
+    s = ' '.join(dic[x] for x in str(i))
+    li.append([i, s])
+li.sort(key = lambda x:x[1])
+
+for i in range(len(li)):
+    if i % 10 == 0 and i != 0:
+        print()
+    print(li[i][0], end = " ")
