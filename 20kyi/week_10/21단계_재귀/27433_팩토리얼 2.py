5962cae6941f6b5c5d905ccb4262ff4cb191c6cb
0521kimyi <0521kimyi@gmail.com>
2024-02-26 20:26:24 +0900

    [BOJ] 27433/팩토리얼 2(수정)/브론즈5

--- 20kyi/week_10/21단계_재귀/27433_팩토리얼 2.py
+++ 20kyi/week_10/21단계_재귀/27433_팩토리얼 2.py
@@ -1,14 +1,24 @@
 # 재귀함수 이용한 팩토리얼
 
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
+
 
 def factorial(t):
     result = 1
     for i in range(1, t + 1):
         result = result * i
     return result
+
+
+# 또는
+# def factorial(n):
+# if(n > 1):
+#     return n * factorial(n - 1)
+# else:
+#     return 1
 
 print(factorial(n))
